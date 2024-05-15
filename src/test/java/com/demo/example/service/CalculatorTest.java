bfa933c4cf495c307b6e5998e0b093d36e5a2b02
Kamal-13-Sing <donbimal383@gmail.com>
2024-05-15 08:33:35 +0545

    second commit

---
+++ src/test/java/com/demo/example/service/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.demo.example.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void addTest(){
+
+        int result = Calculator.add(2,5);
+
+        int expected= 7;
+
+        Assert.assertEquals(expected, result);
+
+    }
+}
